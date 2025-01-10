16ccd4a1e70c6923dcbde25f739b8c0b67fa9078
Your Name <you@example.com>
2025-01-10 14:53:07 +0100

    Solved memory issues Added bitshift right so boolean values are exactly 1 and not != 0 Fixed debug config for unittests on windows

--- Code/include/CanValueTemplate.h
+++ Code/include/CanValueTemplate.h
@@ -17,9 +17,11 @@ class CanValueTemplate
         CanValueTemplate(pugi::xml_node template_description);
         std::vector<uint8_t> parseData(std::vector<uint8_t> can_data);
         std::vector<uint8_t> modifierSelectByte(std::vector<uint8_t> data, uint8_t bit_start, uint8_t bit_end);
-        std::vector<uint8_t> modifierGain(std::vector<uint8_t> , int64_t B);
-        std::vector<uint8_t> modifierBitwiseOr(std::vector<uint8_t> data, int64_t B);
-        std::vector<uint8_t> modifierBitwiseAnd(std::vector<uint8_t> data, int64_t B);
+        std::vector<uint8_t> modifierGain(std::vector<uint8_t> , uint64_t B);
+        std::vector<uint8_t> modifierBitwiseOr(std::vector<uint8_t> data, uint64_t B);
+        std::vector<uint8_t> modifierBitwiseAnd(std::vector<uint8_t> data, uint64_t B);
+        std::vector<uint8_t> modifierBitShiftRight(std::vector<uint8_t> data, uint64_t nr_bits);
+        std::vector<uint8_t> modifierBitShiftLeft(std::vector<uint8_t> data, uint64_t nr_bits);
 
 
 
