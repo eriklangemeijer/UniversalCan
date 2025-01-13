1c9cb315ab0d8e2a8a94fb874b902d2d310d8651
eriklangemeijer <eriklangemeijer@outlook.com>
2025-01-13 14:44:17 +0100

    Reduced functions by including gain in generic function call

--- Code/include/CanValueTemplate.h
+++ Code/include/CanValueTemplate.h
@@ -13,18 +13,17 @@ class CanValueTemplate
         std::string data_type ;
         std::list<std::function<std::vector<uint8_t>(std::vector<uint8_t> data)>> func_list;
 
-        template<typename T> std::vector<uint8_t> applyGain(std::vector<uint8_t> data, uint64_t B);
         template<typename T> std::vector<uint8_t> applyBitShift(std::vector<uint8_t> data, uint64_t nr_bits, bool isLeftShift);
-        template<typename T, typename Op> std::vector<uint8_t> applyBitwiseOperation(std::vector<uint8_t> data, T B, Op op);
+        template<typename T, typename Op> std::vector<uint8_t> applyOperation(std::vector<uint8_t> data, T B, Op operation);
+        template<typename T> void copyTypeToData(T value, std::vector<uint8_t>& data);
+        template<typename T> T convertDataToType(const std::vector<uint8_t>& data);
     public:
         
         CanValueTemplate(pugi::xml_node template_description);
         std::vector<uint8_t> parseData(std::vector<uint8_t> can_data);
         std::vector<uint8_t> modifierSelectByte(std::vector<uint8_t> data, uint8_t bit_start, uint8_t bit_end);
-        std::vector<uint8_t> modifierGain(std::vector<uint8_t> , uint64_t B);
-        template<typename Op> std::vector<uint8_t> modifierBitwise(std::vector<uint8_t> data, uint64_t B, Op op);
+        template<typename Op> std::vector<uint8_t> callOperationForDatatype(std::vector<uint8_t> data, uint64_t B, Op operation);
         std::vector<uint8_t> modifierBitShift(std::vector<uint8_t> data, uint64_t nr_bits, bool isLeftShift);
-
 
 
 };
--- Code/src/CanValueTemplate.cpp
+++ Code/src/CanValueTemplate.cpp
@@ -4,25 +4,9 @@
 #include <functional>
 #include <iostream>
 #include <stdexcept>
-#include <string.h>
 #include <vector>
 
 const uint8_t max_value_size = 8;
-
-template<typename T>
-T convertDataToType(const std::vector<uint8_t>& data) {
-    if (data.size() != sizeof(T)) {
-        throw std::runtime_error("Data size mismatch");
-    }
-    T value;
-    std::memcpy(&value, data.data(), sizeof(T));
-    return value;
-}
-
-template<typename T>
-void copyTypeToData(T value, std::vector<uint8_t>& data) {
-    std::memcpy(data.data(), &value, sizeof(T));
-}
 
 CanValueTemplate::CanValueTemplate(pugi::xml_node template_description)
 {
@@ -32,47 +16,50 @@ CanValueTemplate::CanValueTemplate(pugi::xml_node template_description)
     auto operations = template_description.child("Operations");
     for (auto operation = operations.first_child(); operation; operation = operation.next_sibling()) {
         if(strcmp(operation.name(), "BYTE_SELECT") == 0 ) {
-            uint64_t arg1 = operation.attribute("arg1").as_int();
-            uint64_t arg2 = operation.attribute("arg2").as_int();
-            func_list.emplace_back([this, arg1, arg2](std::vector<uint8_t> data) {
-                return modifierSelectByte(data, arg1, arg2);
-            });
+          uint64_t const arg1 = operation.attribute("arg1").as_int();
+          uint64_t const arg2 = operation.attribute("arg2").as_int();
+          func_list.emplace_back([this, arg1, arg2](std::vector<uint8_t> data) {
+            return modifierSelectByte(data, arg1, arg2);
+          });
         }
         else if(strcmp(operation.name(), "GAIN") == 0 ) {
-            uint64_t arg1 = operation.attribute("arg1").as_int();
-            func_list.emplace_back([this, arg1](std::vector<uint8_t> data) {
-              return modifierGain(data, arg1);
-            });
+          uint64_t const arg1 = operation.attribute("arg1").as_int();
+          func_list.emplace_back([this, arg1](std::vector<uint8_t> data) {
+            return callOperationForDatatype(data, arg1, std::multiplies<>());
+          });
         }
         else if(strcmp(operation.name(), "BITWISE_OR") == 0 ) {
-            uint64_t arg1 = operation.attribute("arg1").as_int();
-            func_list.emplace_back([this, arg1](std::vector<uint8_t> data) {
-              return modifierBitwise(data, arg1, std::bit_or<>());
-            });
+          uint64_t const arg1 = operation.attribute("arg1").as_int();
+          func_list.emplace_back([this, arg1](std::vector<uint8_t> data) {
+            return callOperationForDatatype(data, arg1, std::bit_or<>());
+          });
         }
         else if(strcmp(operation.name(), "BITWISE_AND") == 0 ) {
-            uint64_t arg1 = operation.attribute("arg1").as_int();
-            func_list.emplace_back([this, arg1](std::vector<uint8_t> data) {
-              return modifierBitwise(data, arg1, std::bit_and<>());
-            });
+          uint64_t const arg1 = operation.attribute("arg1").as_int();
+          func_list.emplace_back([this, arg1](std::vector<uint8_t> data) {
+            return callOperationForDatatype(data, arg1, std::bit_and<>());
+          });
         }
         else if(strcmp(operation.name(), "BITSHIFT_RIGHT") == 0 ) {
-            uint64_t arg1 = operation.attribute("arg1").as_int();
-            func_list.emplace_back([this, arg1](std::vector<uint8_t> data) {
-              return modifierBitShift(data, arg1, false);
-            });
+          uint64_t const arg1 = operation.attribute("arg1").as_int();
+          func_list.emplace_back([this, arg1](std::vector<uint8_t> data) {
+            return modifierBitShift(data, arg1, false);
+          });
         }
         else if(strcmp(operation.name(), "BITSHIFT_LEFT") == 0 ) {
-            uint64_t arg1 = operation.attribute("arg1").as_int();
-            func_list.emplace_back([this, arg1](std::vector<uint8_t> data) {
-              return modifierBitShift(data, arg1, true);
-            });
+          uint64_t const arg1 = operation.attribute("arg1").as_int();
+          func_list.emplace_back([this, arg1](std::vector<uint8_t> data) {
+            return modifierBitShift(data, arg1, true);
+          });
         }
         else {
             std::cout << "Unsupported function: " << operation.name() << std::endl;
         }
     }
 }
+
+
+
 
 std::vector<uint8_t> CanValueTemplate::parseData(std::vector<uint8_t> can_data)
 {
@@ -87,36 +74,13 @@ std::vector<uint8_t> CanValueTemplate::modifierSelectByte(std::vector<uint8_t> d
     if(byte_start > byte_end || (byte_end - byte_start) > max_value_size) {
         throw std::runtime_error("byte_start must be smaller than byte_end");
     }
-    return std::vector<uint8_t>(data.begin() + byte_start, data.begin() + byte_end);
+    return { data.begin() + byte_start, data.begin() + byte_end };
 }
 
-template<typename T>
-std::vector<uint8_t> CanValueTemplate::applyGain(std::vector<uint8_t> data, uint64_t B) {
-    T value = convertDataToType<T>(data);
-    value *= B;
-    copyTypeToData(value, data);
-    return data;
-}
-
-std::vector<uint8_t> CanValueTemplate::modifierGain(std::vector<uint8_t> data, uint64_t B)
-{
-    switch(data.size()) {
-        case sizeof(uint8_t):
-            return applyGain<uint8_t>(data, B);
-        case sizeof(uint16_t):
-            return applyGain<uint16_t>(data, B);
-        case sizeof(uint32_t):
-            return applyGain<uint32_t>(data, B);
-        case sizeof(uint64_t):
-            return applyGain<uint64_t>(data, B);
-        default:
-            throw std::runtime_error("data must be exactly the size of a default integer type (1,2,4 or 8 bytes)");
-    }
-}
 
 
 template<typename T, typename Op>
-std::vector<uint8_t> CanValueTemplate::applyBitwiseOperation(std::vector<uint8_t> data, T B, Op op) {
+std::vector<uint8_t> CanValueTemplate::applyOperation(std::vector<uint8_t> data, T B, Op op) {
     T value = convertDataToType<T>(data);
     value = op(value, static_cast<T>(B));  // Apply the bitwise operation using the passed operation
     copyTypeToData(value, data);
@@ -124,17 +88,17 @@ std::vector<uint8_t> CanValueTemplate::applyBitwiseOperation(std::vector<uint8_t
 }
 
 template<typename Op>
-std::vector<uint8_t> CanValueTemplate::modifierBitwise(std::vector<uint8_t> data, uint64_t B, Op op)
+std::vector<uint8_t> CanValueTemplate::callOperationForDatatype(std::vector<uint8_t> data, uint64_t B, Op op)
 {
     switch (data.size()) {
         case sizeof(uint8_t):
-            return applyBitwiseOperation<uint8_t>(data, (uint8_t)B, op);
+            return applyOperation<uint8_t>(data, (uint8_t)B, op);
         case sizeof(uint16_t):
-            return applyBitwiseOperation<uint16_t>(data, (uint16_t)B, op);
+            return applyOperation<uint16_t>(data, (uint16_t)B, op);
         case sizeof(uint32_t):
-            return applyBitwiseOperation<uint32_t>(data, (uint32_t)B, op);
+            return applyOperation<uint32_t>(data, (uint32_t)B, op);
         case sizeof(uint64_t):
-            return applyBitwiseOperation<uint64_t>(data, (uint64_t)B, op);
+            return applyOperation<uint64_t>(data, (uint64_t)B, op);
         default:
             throw std::runtime_error("data must be exactly the size of a default integer type (1,2,4 or 8 bytes)");
     }
@@ -168,3 +132,17 @@ std::vector<uint8_t> CanValueTemplate::modifierBitShift(std::vector<uint8_t> dat
     }
 }
 
+template<typename T>
+T CanValueTemplate::convertDataToType(const std::vector<uint8_t>& data) {
+    if (data.size() != sizeof(T)) {
+        throw std::runtime_error("Data size mismatch");
+    }
+    T value;
+    std::memcpy(&value, data.data(), sizeof(T));
+    return value;
+}
+
+template<typename T>
+void CanValueTemplate::copyTypeToData(T value, std::vector<uint8_t>& data) {
+    std::memcpy(data.data(), &value, sizeof(T));
+}
