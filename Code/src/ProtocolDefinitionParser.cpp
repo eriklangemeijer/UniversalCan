1237d3d04839a3a0b52775d18096e1bfe0feec46
eriklangemeijer <eriklangemeijer@outlook.com>
2025-01-13 11:14:21 +0100

    Added includes to protocoldefinitionparser. Does not work yet on windows so moving it to MACOS to test

--- Code/src/ProtocolDefinitionParser.cpp
+++ Code/src/ProtocolDefinitionParser.cpp
@@ -1,7 +1,8 @@
 #include <ProtocolDefinitionParser.h>
+#include <experimental/filesystem> // For file path manipulation
+#include <stdexcept>  // For runtime_error
 
-
-
+namespace fs = std::experimental::filesystem;
 
 ProtocolDefinitionParser::ProtocolDefinitionParser(std::string filename) {
     pugi::xml_document doc;
@@ -9,8 +10,22 @@ ProtocolDefinitionParser::ProtocolDefinitionParser(std::string filename) {
     if (!result)
         throw std::runtime_error("Cannot open protocol file");
 
+
     for (auto message = doc.child("ProtocolDefintion").child("Message"); message; message = message.next_sibling("Message")) {
         message_list.emplace_back(CanMessageTemplate(message));
     }
+    fs::path base_path = fs::absolute(fs::path(filename)).parent_path();
 
+    for (auto included_document = doc.child("ProtocolDefintion").child("include"); included_document; included_document = included_document.next_sibling("include")) {
+        std::string relative_path = included_document.attribute("path").as_string();
+
+        fs::path absolute_path = base_path / relative_path;
+        absolute_path = fs::absolute(absolute_path);
+
+        if (!fs::exists(absolute_path)) {
+            throw std::runtime_error("Included file does not exist: " + absolute_path.string());
+        }
+        ProtocolDefinitionParser parent_protocol = ProtocolDefinitionParser(absolute_path.string());
+        // message_list.splice(message_list.end(), parent_protocol.message_list);
+    }
 }
